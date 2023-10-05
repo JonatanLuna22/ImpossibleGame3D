2b1f03ca89afb1154af836014dd33bdd9544c438
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-05 14:42:02 +0200

    Create Start game button

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class StartGameButton : MonoBehaviour
+{
+    public void hoverstart()
+    {
+        transform.localScale = Vector3.one * 1.1f;
+    }
+    public void hoverend()
+    {
+        transform.localScale = Vector3.one;
+    }
+    public void clickstart()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        mr.material.color = Color.grey;
+    }
+    public void clickend()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        mr.material.color = Color.white;
+    }
+    public void click()
+    {
+        
+    }
+    
+}
