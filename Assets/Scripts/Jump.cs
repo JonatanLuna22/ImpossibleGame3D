06135d6ddd8267f4d57a98749396a4d1ebc0f911
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-05 11:52:26 +0200

    Add jump

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Jump
+    {
+        
+    }
+}
+
+void update()
+{
+    if (Input.GetButtonDown("Jump")) // && GameObject (esta en contacto con el suelo o no esta en el aire)
+    {
+        float jumpforce = 300;
+        Rigidbody rigidbody = GameObject.GetComponent<Rigidbody>();
+        rigidbody.AddForce(0,jumpforce, 0);
+    }
+}
--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -14,6 +14,18 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Jump")) 
+            // Jump sucede por la modificacion de dos variables. fuerza hacia arriba y grabedad.
+            // La masa no afecta en la caida pero en la fuerza hacia arriba.
+            // && GameObject (esta en contacto con el suelo o no esta en el aire)
+        {
+            float jumpforce = 300;
+            Rigidbody rigidbody = GameObject.GetComponent<Rigidbody>();
+            rigidbody.AddForce(0,jumpforce, 0);
+        }
         transform.Translate(0,0,speed);
     }
 }
+// Por que se mueve hacia adelante cuando salta si el transform es un else?
+// Por que 300 no lleva un f?
+// por que speed es un public variable but jumpforce is not?
