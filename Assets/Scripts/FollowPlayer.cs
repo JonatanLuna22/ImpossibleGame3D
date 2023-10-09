ad8a361860e4d9dce79f3b66e32a06b61934a024
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-09 11:08:10 +0200

    Create Follow player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class FollowPlayer : MonoBehaviour
+    {
+        private void LateUpdate()
+        {
+            GameObject player = GameObject.Find("Player");
+            Vector3 target = player.transform.position + new Vector3(0, 0, -10);
+            transform.position = target;
+        }
+    }
+}
--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour
 {
-    public float speed = 0.02f; // A variable that is outside a function and is public, is called field.
-    public float jumpforce = 300;                         // Start is called before the first frame update
+    public float speed = 0.02f;             // A variable that is outside a function and is public, is called field.
+    public float jumpforce = 300;           // Start is called before the first frame update
     public float gravity;
+    public float jumpspin;
+    public float fallgravity = -2;
     void Start()
     {
+        // Application.targetFrameRate = targetFrameRate;
         Physics.gravity = new Vector3(0, gravity, 0);
     }
+    
 
     private void FixedUpdate()      // Esta funcion tiene que ver con caer mas rapido al saltar.
     {
         Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
-        if (rigidbody.velocity.y < -.1f)
+        if (rigidbody.velocity.y < - 1f)
         {
-            rigidbody.AddForce(0, -2, 0);
+            rigidbody.AddForce(0, fallgravity, 0);
         }
+
+        rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, speed);
+        // transform.Translate(0,0,speed * Time.fixedDeltaTime, Space.World); Nos cargamos esto de aqui para sustituirlo por lo que esta justo arriva.
+        // Aqui multiplicamos el speed por Time.fixedDeltaTime para que la velocidad del player este ligada al tiempo que ha pasado desde el ultimo update.
+        // Y lo hemos movido de Udate a FixedUpdate para obligar al PC ha hacer la cosa (translate = movement) 15 veces por segundo.
+        // Asi un pc lento simulara las 15 translaciones aunque no se puedan ver en pantalla. Lo que hace que no se salta la trampa. que te la comas aunque parezca que se la salta.
     }
 
     // Update is called once per frame
@@ -35,7 +45,6 @@ public class Playercontroller : MonoBehaviour
             rigidbody.AddForce(0,jumpforce, 0);
             //rigidbody.angularVelocity = new Vector3(2, 0, 0);     // Este es el giro al saltar.
         }
-        transform.Translate(0,0,speed, Space.World);
     }
 
     bool istouchingground()
@@ -50,4 +59,5 @@ public class Playercontroller : MonoBehaviour
 // Por que speed es un public variable but jumpforce is not? Al estar fuera de la funcion, hago que la variable funcione
 // en funciones mas adelante. Al hacerla publica hago que pueda modificarla later en unity.
 
-// Lo que sea que tengas que hacer con la camara, tiene que ver con la ubicacion inicial del player.
+// Lo que sea que tengas que hacer con la camara, tiene que ver con la ubicacion inicial del player
+// Input funciona por cada frame no se que. Las fisicas funcionan deben ir en Fixupdates.
