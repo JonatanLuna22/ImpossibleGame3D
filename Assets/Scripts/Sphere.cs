db3d8f94f3d44b62da0eed07b15cc32fe697f063
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-10 09:43:07 +0200

    Create jump Sphere

---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpPad : MonoBehaviour
+{
+    public float jumpForce = 20;
+    private void OnCollisionEnter(Collider other)
+    {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        Vector3 velocity = rb.velocity;
+        velocity.y = jumpForce;
+        rb.velocity = velocity;
+    }
+}
